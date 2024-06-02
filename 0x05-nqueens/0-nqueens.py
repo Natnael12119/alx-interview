85ff0c2e51685f0eee378e6d30fadc6b9ec83ba1
Natnael12119 <nhattydeveloper@gmail.com>
2024-06-02 00:57:53 +0000

    Debugging

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 import sys
 
+
 def print_solution(board):
     solution = []
     for i in range(len(board)):
@@ -9,56 +10,48 @@ def print_solution(board):
                 solution.append([i, j])
     print(solution)
 
+
 def is_safe(board, row, col):
-    # Check this row on the left side
     for i in range(col):
         if board[row][i] == 1:
             return False
-
-    # Check upper diagonal on the left side
     for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
         if board[i][j] == 1:
             return False
-
-    # Check lower diagonal on the left side
     for i, j in zip(range(row, len(board), 1), range(col, -1, -1)):
         if board[i][j] == 1:
             return False
-
     return True
+
 
 def solve_nqueens(board, col):
     if col >= len(board):
         print_solution(board)
         return True
-
     res = False
     for i in range(len(board)):
         if is_safe(board, i, col):
             board[i][col] = 1
             res = solve_nqueens(board, col + 1) or res
-            board[i][col] = 0  # backtrack
-
+            board[i][col] = 0
     return res
+
 
 def nqueens(n):
     board = [[0 for _ in range(n)] for _ in range(n)]
-    if not solve_nqueens(board, 0):
-        print("Solution does not exist")
+    solve_nqueens(board, 0)
+
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
         print("Usage: nqueens N")
         sys.exit(1)
-    
     try:
         n = int(sys.argv[1])
     except ValueError:
         print("N must be a number")
         sys.exit(1)
-
     if n < 4:
         print("N must be at least 4")
         sys.exit(1)
-    
     nqueens(n)
