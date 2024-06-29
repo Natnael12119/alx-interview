7e2b0f81b9041ba24f268c5ca24f7d88b58feffd
Natnael12119 <nhattydeveloper@gmail.com>
2024-06-29 21:17:02 +0000

    Debugging

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -1,17 +1,31 @@
 #!/usr/bin/python3
-"""
-Island Perimeter
-"""
+'''island perimeter
+'''
+
+
+def numofIsland(grid, i, j):
+    '''Num of islands
+    '''
+    count = 0
+    if (i > 0 and grid[i - 1][j]):
+        count += 1
+    if (j > 0 and grid[i][j - 1]):
+        count += 1
+    if (i < len(grid)-1 and grid[i + 1][j]):
+        count += 1
+    if (j < len(grid[0])-1 and grid[i][j + 1]):
+        count += 1
+    return count
 
 
 def island_perimeter(grid):
-    """
-     returns the perimeter of the island described in grid
-    :param grid:
-    :return:
-    """
-    area = 0
-    for row in grid + list(map(list, zip(*grid))):
-        for i1, i2 in zip([0] + row, row + [0]):
-            area += int(i1 != i2)
-    return area
+    '''perimeter function
+    '''
+    C = len(grid[0])
+    R = len(grid)
+    perimeter = 0
+    for i in range(0, R):
+        for j in range(0, C):
+            if (grid[i][j]):
+                perimeter += (4 - numofIsland(grid, i, j))
+    return perimeter
